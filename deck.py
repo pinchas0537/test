c89ef46db1b5e8783cb0d37d887a00a60aec77aa
Pinchas Lifshitz <pnl8480537@gmail.com>
2025-10-26 15:28:35 +0200

    added creat_cardfanction

--- deck.py
+++ deck.py
@@ -1,1 +1,3 @@
-def 
+def create_card(rank:str, suite:str) -> dict:
+    card = {"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":11,"Q":12,"K":13,"A":14}
+    return {"rank":rank ,"suite":suite ,"value":card[rank]}
